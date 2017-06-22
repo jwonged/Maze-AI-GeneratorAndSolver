7ee920f8e7351399d42c33edb4d451dbea085a06
Joshua Wong <dsjw2@cam.ac.uk>
2017-06-22 10:55:11 +0100

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -7,27 +7,34 @@ import java.util.Stack;
 public class Maze {
 	private ArrayList<ArrayList<Cell>> maze;
 	private ArrayList<ArrayList<Index>> dual;
-	Cell origin;
-	Cell end;
-	private int x,y;//width and height
+	private int x,y; 
+	public Cell origin;
+	public Cell end;
 	
 	public Maze(int x, int y) {
 		this.x = x;
 		this.y = y;
 		
-		//initialize cells
+		//initialize cells and dual
 		maze = new ArrayList<ArrayList<Cell>>(x);
-		for (int j=0; j<x; j++) {
-			maze.add(new ArrayList<Cell>(y));
-			for (int i=0; i<y; i++) {
-				maze.get(j).add(new Cell(j,i));
+		dual = new ArrayList<ArrayList<Index>>(x+1);
+		
+		for (int j=0; j<=x; j++) {
+			if (j<x) maze.add(new ArrayList<Cell>(y));
+			dual.add(new ArrayList<Index>(y+1));
+			
+			for (int i=0; i<=y; i++) {
+				if (i<y && j<x) maze.get(j).add(new Cell(j,i));
+				dual.get(j).add(new Index(j,i));
 			}
+			
 		}
+		
+		origin = maze.get(0).get(0);
 		
 		//pre-compute neighbours
 		initNeighbours();
 	}
-	
 	private void initNeighbours() {
 		//precompute the 4 physical neighbours around cell
 		for (ArrayList<Cell> row : maze) {
@@ -39,25 +46,108 @@ public class Maze {
 			}
 		}
 	}
+	public void initDual(Grid grid) {
+		//Create a dual graph
+		for (ArrayList<Index> row : dual) {
+			for (Index c : row) {
+				if (c.row > 0) c.connectTo.add(dual.get(c.row-1).get(c.col));
+				if (c.col > 0) c.connectTo.add(dual.get(c.row).get(c.col-1));
+				if (c.row < x) c.connectTo.add(dual.get(c.row+1).get(c.col));
+				if (c.col < y) c.connectTo.add(dual.get(c.row).get(c.col+1));
+			}
+		}
+		//send all edges to GUI
+		for (ArrayList<Index> row : dual) {
+			for (Index c : row) {
+				for (Index i : c.connectTo) grid.addEdge(new Edge(c,i));
+			}
+		}
+		
+	}
 	
 	public void iterate() {
 		System.out.println(maze.size());
 		for (ArrayList<Cell> row : maze) {
 			for (Cell c : row) {
 				System.out.print("("+c.x+","+c.y+")");
-				for (Cell n : c.neighbours)
-					System.out.print("("+n.x+","+n.y+")");
+				for (Cell n : c.neighbours) System.out.print("("+n.x+","+n.y+")");
 				System.out.println();
-
 			}
 			System.out.println();
 		}
 	}
 	
+	public void dfsgenerator(Grid grid) {
+		//dfs maze
+		Stack<Cell> stack = new Stack<Cell>();
+		stack.add(origin);
+		origin.visited = true;
+		
+		while (!stack.isEmpty()) {
+			Cell current = stack.pop();
+			for (Cell n : current.neighbours) {
+				if (!n.visited) {
+					n.visited = true;
+					stack.push(n);
+					current.paths.add(n);
+					grid.removeEdge(current,n);
+					
+				}
+			}
+		}
+		System.out.println("end");
+	}
 	
+	public void dfsi (Grid grid, Cell current, Cell parent) {
+		if (current.y == this.y-1) end = current;
+		if (current.visited || current == null) return;
+		
+		System.out.println("Visiting: (" +current.x+","+current.y+")");
+		current.visited = true;
+		if (parent != null) {
+			System.out.println("Removing: (" +current.x+","+current.y+")"+" parent: ("+parent.x+","+parent.y+")");
+			grid.removeEdge(current,parent);
+			parent.paths.add(current);
+		}
+		
+		for (Cell n : current.neighbours) {
+			if (!n.visited) dfsi(grid,n, current);
+			
+		}
+		
+		
+	}
 	
-	public void generateMaze() {
+	public void generateMaze(Grid grid) {
+		helper(maze.get(0).get(0),grid,null);
+		System.out.print("End: "+end.x+","+end.y);
+		grid.addStartEnd(origin,end);
+		
+	}
+	
+	private void helper(Cell current, Grid grid, Cell parent) {
+		if (current.y == this.y-1) end = current;
+		if (current.visited || current == null) return;
+
+		Random rand = new Random();
+		current.visited = true;
+		if (parent != null) grid.removeEdge(current,parent); 
+		
+		Cell n;
+		while(!current.neighbours.isEmpty()) {
+			int randNum = rand.nextInt(current.neighbours.size());
+
+			n = current.neighbours.remove(randNum);
+			
+			current.paths.add(n);
+			helper(n,grid, current);
+			
+		}
+	}
+	
+	public void backTrackmaze(Grid grid) {
 		//remove all neighbours, leave only possible paths
+		//iterative w stack
 		Random rand = new Random();
 		origin = maze.get(0).get(0);
 		Stack<Cell> stack = new Stack<Cell>();
@@ -65,21 +155,25 @@ public class Maze {
 		
 		while (!stack.isEmpty()) {
 			Cell current = stack.pop();
-			Cell check;
+			Cell n;
 			
 			current.visited = true;
 			if (current.y == this.y-1) end = current;
 			
 			//Take rand neighbour: if not visited, add as path and push to stack
-			while (current.neighbours.size()>0) {
+			while (!current.neighbours.isEmpty()) {
 				int randNum = rand.nextInt(current.neighbours.size());
-				if (!(check = current.neighbours.remove(randNum)).visited) {
-					current.paths.add(check);
-					stack.push(check);
+				n = current.neighbours.remove(randNum);
+				
+				if (!n.visited) {
+					n.visited = true;
+					current.paths.add(n);
+					 grid.removeEdge(current,n);
+					stack.push(n);
 				}
 			}
-			
 		}
+		
 		
 	}
 	
